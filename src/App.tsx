c6868d58b4cf4b934f72fc95ea248369ed7bc6e8
Jett <jettc505@gmail.com>
2023-06-19 12:45:48 -0600

    _

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import './App.css'
+import { useEffect } from "react"
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import NavBar from "./components/Navbar";
+import AboutMe from "./pages/AboutMe";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio"
 
 function App() {
-  return (
-    <>
-        <h1>Jett is awesome</h1>
-        <div></div>
-    </>
-  )
+
+    useEffect 
+ 
+    return (
+        <BrowserRouter>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<AboutMe />} />
+                <Route path="/aboutme" element={<AboutMe />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/resume" element={<Resume />} />
+            </Routes> 
+        </BrowserRouter>      
+    )
 }
 
 export default App
